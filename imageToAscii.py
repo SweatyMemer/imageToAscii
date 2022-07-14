f3ed4782dacd6bae86be8f77759b3d8a7e56c330
SweatyMemer <ds123shep@gmail.com>
2022-07-14 20:37:29 +1000

    added video support and webcam support, also inverted the colours to make it look better

--- imageToAscii.py
+++ imageToAscii.py
@@ -1,13 +1,17 @@
 from asyncio.windows_events import NULL
 from PIL import Image
 import os
+import cv2
 from time import sleep
-from colorama import Style, init, deinit
+from colorama import Style, init, deinit, reinit, Fore
+from keyboard import is_pressed
+from datetime import datetime, timedelta
 init()
-ASCII_CHARS = list("@#S%?*+;:,.")
+# ASCII_CHARS = list("@#S%?*+;:,.")
+ASCII_CHARS = list(".,:;+*?%S$@")
 
 desired_width = 500
-
+webcam_width = 300
 class colours:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -18,6 +22,7 @@ class colours:
     ENDC = '\033[0m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
+    ORANGE = '\033[208m'
 
 
 def terminalSize(width=50, height=20):
@@ -25,10 +30,10 @@ def terminalSize(width=50, height=20):
     os.system('mode {}, {}'.format(width, height))
 
 def terminalDefault(): #  returns to the default terminal size (kinda of, but not really, it's still a little weird and i cba)
-    changeFontSize(16)
-    terminalSize(70,30)
+    changeFontSize(8,16)
+    terminalSize(120,30)
 
-def changeFontSize(size=2): #Changes the font size to *size* pixels (kind of, but not really. You'll have to try it to chack if it works for your purpose ;) )
+def changeFontSize(xSize = 2, ySize = 2): #Changes the font size to *size* pixels (kind of, but not really. You'll have to try it to chack if it works for your purpose ;) )
     from ctypes import POINTER, WinDLL, Structure, sizeof, byref
     from ctypes.wintypes import BOOL, SHORT, WCHAR, UINT, ULONG, DWORD, HANDLE
 
@@ -73,8 +78,8 @@ def changeFontSize(size=2): #Changes the font size to *size* pixels (kind of, bu
     font = CONSOLE_FONT_INFOEX()
     font.cbSize = sizeof(CONSOLE_FONT_INFOEX)
 
-    font.dwFontSize.X = size
-    font.dwFontSize.Y = size
+    font.dwFontSize.X = xSize
+    font.dwFontSize.Y = ySize
 
     set_current_console_font_ex_func(stdout, False, byref(font))
     
@@ -83,24 +88,33 @@ def changeFontSize(size=2): #Changes the font size to *size* pixels (kind of, bu
 
 
 
-def list_Dir(path = 'CWD'):
+def list_Dir(path = 'CWD', filetypes = ['.jpg', '.png']):
 
     # print(colours.HEADER + " === Available files === \n === " + colours.ENDC + colours.OKGREEN + "")
     print(f"{colours.HEADER} === Available files === \n ==={colours.ENDC}{colours.OKGREEN} Green{colours.ENDC}{colours.HEADER} files are valid images ==={colours.ENDC}")
 
     if path == 'CWD':
         for file in os.listdir():
-            if file.endswith(".jpg") or file.endswith(".png"):
-                print(colours.OKGREEN + file + colours.ENDC)
+            # if file.endswith(".jpg") or file.endswith(".png"):
+            for filetype in filetypes:
+                if file.endswith(filetype):
+                    print(colours.OKGREEN + file + colours.ENDC)
+                    break
             # if file.endswith(".jpg") or file.endswith(".png"):
             #     print(file)
             else:
                 print(file)
     else:
         for file in os.listdir(path):
-            if file.endswith(".jpg") or file.endswith(".png"):
+            for filetype in filetypes:
+                if file.endswith(filetype):
+                    print(colours.OKGREEN + file + colours.ENDC)
+                    break
+            # if file.endswith(".jpg") or file.endswith(".png"):
+            #     print(file)
+            else:
                 print(file)
-
+                
 def resize_image(image, new_width=desired_width):
     width, height = image.size
     ratio = height/width
@@ -154,11 +168,118 @@ def imageToAscii(new_width = desired_width):
 
     with open("ascii_image_txt", 'w') as f:
         f.write(ascii_image)
+        
+    input()
+    terminalDefault()   
 
+def findFps(video):
+    (major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')
+    if int(major_ver)  < 3 :
+        fps = video.get(cv2.cv.CV_CAP_PROP_FPS)
+        print("Frames per second using video.get(cv2.cv.CV_CAP_PROP_FPS): {0}".format(fps))
+    else :
+        fps = video.get(cv2.CAP_PROP_FPS)
+        print("Frames per second using video.get(cv2.CAP_PROP_FPS) : {0}".format(fps))
+        
+    return(fps)
 
-# def imgFolderToAscii(new_width = desired_width)::
+def videoToAscii(new_width = desired_width):
+    list_Dir('CWD', ['mp4'])
+    path = input("Path: ")
 
-imageToAscii()
+    videoOpened = False
+    while not videoOpened:
+        try:
+            video = cv2.VideoCapture(path)
+            videoOpened = True
+        except:
+            print(f"couldn't open {path}")
+            path = input("Path: ")
 
+    success, cv2image = video.read()
+    image = Image.fromarray(cv2image)
+    
+    count = 0
+    changeFontSize(2)
+    terminalSize(new_width, round(new_width * get_ratio(image)))
+    deinit()
+    
+    time1 = datetime.now()
+    
+    while success and not is_pressed('enter'):
+        success, cv2image = video.read()
+        if success:
+            image = Image.fromarray(cv2image)
+        else: 
+            break
+        
+        print("Read a new frame: " + str(success))
+        
+        new_image_data = pixels_to_ascii(greyify(resize_image(image)))   
+        pixel_count = len(new_image_data)
+        ascii_image = "\n".join(new_image_data[i:(i+new_width)] for i in range(0, pixel_count, new_width))
+        
+        print(ascii_image)
+        count += 1
+
+    time2 = datetime.now()
+    elapsedTime = time2 - time1
+    terminalDefault()
+    reinit()
+    print(f"Average of {colours.OKCYAN}{count/elapsedTime.total_seconds()}{colours.ENDC}fps over {colours.OKCYAN}{divmod(elapsedTime.total_seconds(), 60)[0]}{colours.ENDC} minutes and {colours.OKCYAN}{divmod(elapsedTime.total_seconds(), 60)[1]}{colours.ENDC} seconds")
+    # os.mkdir("frames")
+    
+def webcamToAscii(new_width = webcam_width):
+    cap = cv2.VideoCapture(0)
+    success, cv2image = cap.read()
+    image = Image.fromarray(cv2image)
+    with open("log.txt", 'w') as log:
+        print(round(get_ratio(image)), file=log)
+    count = 0
+    changeFontSize(2)
+    terminalSize(new_width, round(new_width * get_ratio(image)))
+    deinit()
+    
+    time1 = datetime.now()
+    
+    while success and not is_pressed('enter'):
+        success, cv2image = cap.read()
+        if success:
+            image = Image.fromarray(cv2image)
+        else: 
+            break
+        
+        print("Read a new frame: " + str(success))
+        
+        new_image_data = pixels_to_ascii(greyify(resize_image(image, webcam_width)))   
+        pixel_count = len(new_image_data)
+        ascii_image = "\n".join(new_image_data[i:(i+new_width)] for i in range(0, pixel_count, new_width))
+        
+        print(ascii_image)
+        count += 1
+
+    time2 = datetime.now()
+    elapsedTime = time2 - time1
+    terminalDefault()
+    reinit()
+    print(f"Average of {colours.OKCYAN}{count/elapsedTime.total_seconds()}{colours.ENDC}fps over {colours.OKCYAN}{divmod(elapsedTime.total_seconds(), 60)[0]}{colours.ENDC} minutes and {colours.OKCYAN}{divmod(elapsedTime.total_seconds(), 60)[1]}{colours.ENDC} seconds")
+    # os.mkdir("frames")
+    
+def main():
+    
+    prompt = f"1) {Fore.YELLOW}image to ascii{colours.ENDC}\n2) {Fore.YELLOW}video to ascii {colours.ENDC} \n3) {Fore.YELLOW}webcam to ascii {colours.ENDC}\n{colours.HEADER}=>{colours.ENDC}"
+    print(prompt, end="")
+    textIn = input()
+    # textIn = input(prompt)
+    if textIn == "1":
+        imageToAscii()
+        
+    elif textIn == "2":
+        videoToAscii()
+
+    elif textIn == "3":
+        webcamToAscii()
+        
+        
+main()
 input()
-terminalDefault()
